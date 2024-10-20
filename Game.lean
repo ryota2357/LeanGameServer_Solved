d001645ef34495acf12306cd4cb06543b7efa08c
ryota2357 <61523777+ryota2357@users.noreply.github.com>
2024-10-20 20:02:34 +0900

    done: STG subset world

--- Game.lean
+++ Game.lean
@@ -1,1 +1,2 @@
 import Game.NaturalNumber
+import Game.SetTheory
---
+++ Game/SetTheory/World/Subset.lean
@@ -0,0 +1,38 @@
+import Mathlib.Data.Set.Basic
+
+namespace Game.SetTheory.Subset
+
+variable {U : Type}
+
+-- 1
+example (x : U) (A : Set U) (h : x ∈ A) : x ∈ A := by
+  exact h
+
+-- 2
+example (x : U) (A B : Set U) (h1 : A ⊆ B) (h2 : x ∈ A) : x ∈ B := by
+  exact h1 h2
+
+-- 3
+example (x : U) (A B C : Set U) (h1 : A ⊆ B) (h2 : B ⊆ C) (h3 : x ∈ A) : x ∈ C := by
+  -- exact h2 (h1 h3)
+  have h4 : x ∈ B := h1 h3
+  exact h2 h4
+
+-- 3
+example {x : U} {A B C : Set U} (h1 : A ⊆ B) (h2 : x ∈ B → x ∈ C) : x ∈ A → x ∈ C := by
+  -- intro h3
+  -- apply h2
+  -- exact h1 h3
+  intro h3
+  exact h2 (h1 h3)
+
+-- 4
+theorem refl (A : Set U) : A ⊆ A := by
+  intro x
+  intro h
+  exact h
+
+-- 5
+theorem trans {A B C : Set U} (h1 : A ⊆ B) (h2 : B ⊆ C) : A ⊆ C := by
+  intro x h
+  exact h2 (h1 h)
